55803e92208545a83b15be3a9cb3f7864616cca2
Andrew Pahl <andrewpahl1@gmail.com>
2022-02-09 21:48:04 -0700

    added the ability to kill/revive cells by clicking on cell in the gui

--- gui.py
+++ gui.py
@@ -12,6 +12,9 @@ class GUI:
         self.game = game_of_life
         self.cell_size = 10
         self.running = False
+        self.updating = False
+        self.cell_painting = False
+        self.cell_painting_state = True
         self.window_height = self.game.height * (self.cell_size + 1) + 1
         self.window_width = self.game.width * (self.cell_size + 1) + 1
         pygame.init()
@@ -28,21 +31,42 @@ class GUI:
                 pygame.draw.rect(self.screen, cell_color, rect, 0)
     
     def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
-        if event.type == pygame.MOUSEBUTTONUP:
-            pass
-        if event.type == pygame.KEYDOWN:
+        if event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN):
+            cell_pos = self.get_mouse_cell_pos()
+        if event.type == pygame.MOUSEBUTTONUP and cell_pos:
+            self.cell_painting = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and cell_pos:
+            self.cell_painting_state = not self.game.grid[cell_pos[0]][cell_pos[1]]
+            self.cell_painting = True
+        elif event.type == pygame.KEYDOWN:
             if event.unicode == " ":
-                self.game.update_all()
+                self.updating = not self.updating
         elif event.type == pygame.QUIT:
             self.running = False
+    
+    def get_mouse_cell_pos(self):
+        x, y = pygame.mouse.get_pos()
+        within_cell_bounds_x = x < self.game.width * (self.cell_size + 1) + 1
+        within_cell_bounds_y = y < self.game.height * (self.cell_size + 1) + 1
+        if not within_cell_bounds_x or not within_cell_bounds_y:
+            return None
+        cell_y = int(y // ((self.game.height * (self.cell_size + 1) + 1) / self.game.height))
+        cell_x = int(x // ((self.game.width * (self.cell_size + 1) + 1) / self.game.width))
+        return (cell_y, cell_x)
+    
+    def paint_cells(self):
+        cell_pos = self.get_mouse_cell_pos()
+        self.game.update_state(cell_pos, self.cell_painting_state)
     
     def run_game(self):
         self.running = True
         while self.running:
             for event in pygame.event.get():
                 self.handle_event(event)
+            if self.cell_painting:
+                self.paint_cells()
+            if self.updating:
+                self.game.update_all()
             self.screen.fill(GREY)
             self.draw_field()
             pygame.display.flip()
